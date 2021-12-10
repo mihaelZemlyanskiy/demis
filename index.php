2b01ef02614d8823d98a53de455ab46da5dd65b1
mihaelZemlyanskiy <73349068+mihaelZemlyanskiy@users.noreply.github.com>
2021-12-10 18:01:03 +0300

    Update index.php

--- index.php
+++ index.php
@@ -14,15 +14,27 @@
 				require_once 'connect.php';
 			?>
 	<div>
-		<form id='form' action='' method='post'>
-			<div><span>ФИО</span><input type="text" name="fullname" placeholder="Иванов Иван Иваныч" pattern="[А-Яа-яЁё]{2,15}\s[А-Яа-яЁё]{2,15}\s[А-Яа-яЁё]{2,30}"></div>
-			<div><span>Адрес</span><input type="text" name="address"placeholder="Тамбов Советская 33 кв 159"></div>
-			<div><span>Телефон</span><input type="tel" name="phone"placeholder="7894561532" pattern="[0-9]{10}"></div>
-			<div><span>Почта</span><input type="email" name="mail"placeholder="user@gmail.com" ></div>
+		<div class="messageForm">
+			<div>		
+				<div class="formError">
+					<div>
+						<p>Ошибка при заполнении формы.</p>
+						<p>Пожалуйста, попробуйте еще раз.</p>
+					</div>
+					<p class="clearmessage">Закрыть</p>	
+				</div>
+			</div>
+		</div>
+
+		<form id='form' action='' method='post'><!--formHandler.php-->
+			<div><span>ФИО</span><span class="messageError fullname none">Это поле обязательно</span><input type="text" name="fullname" placeholder="Иванов Иван Иваныч" pattern="[А-Яа-яЁё]{2,15}\s[А-Яа-яЁё]{2,15}\s[А-Яа-яЁё]{2,30}"></div>
+			<div><span>Адрес</span><span class="messageError address none">Это поле обязательно</span><input type="text" name="address"placeholder="Тамбов Советская 33 кв 159"></div>
+			<div><span>Телефон</span><span class="messageError phone none">Это поле обязательно</span><input type="tel" name="phone"placeholder="8(___) ___-____" pattern="[0-9]{10}"></div>
+			<div><span>Почта</span><span class="messageError mail none">Это поле обязательно</span><input type="email" name="mail"placeholder="user@gmail.com"></div>
 			<div id='sub'><input id='button' type='submit' name='sub'></div>	
 		</form>
 
-		<table class="resultTable">
+		<table>
 			<tr>
 				<th>ФИО</th><th>Адрес</th><th>Телефон</th><th>Почта</th>
 			</tr>
@@ -42,7 +54,7 @@
 				echo '</tr>';
 			}
 			?>
-			
+			<!--<tr id='result_form'></tr>-->
 
 		</table>
 
@@ -50,6 +62,7 @@
 
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
 	<script src='script/script.js'></script>
+	<script src="script/jquery.maskedinput.min.js"></script>
 
 	</body>
 </html>
